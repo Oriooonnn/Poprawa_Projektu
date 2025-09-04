0fb0dbb85f62fad0fb8e0150ca9af3d15a1227b4
Oriooonnn <jasko.kusnierz@vp.pl>
2025-09-04 20:58:25 +0200

    Szkielet projektu poprawkowego

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
     do {
         System.out.println("==MENU GLOWNE SYMULATORA APLIKACJI SPOTIFY==");
         System.out.println("1. Dodaj nowa piosenke");
-        System.out.println("2. Wyswietl dostepne utwory");
-        System.out.println("3. Utworz nowa playliste");
-        System.out.println("4. Dodaj utwor do playlisty");
-        System.out.println("5. Wyswietl dostepne playlisty");
+        System.out.println("2. Wyswietl dostepne utwory.");
+        System.out.println("3. Utworz nowa playliste.");
+        System.out.println("4. Dodaj utwor do playlisty.");
+        System.out.println("5. Wyswietl dostepne playlisty.");
         System.out.println("6. Odtworz wybrany utwor.");
         System.out.println("Wybierz opcje.");
         opcja = scanner.nextInt();
