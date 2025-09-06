8c5926d330dcc0529076399ae45a30d7bcb879a3
Oriooonnn <jasko.kusnierz@vp.pl>
2025-09-06 18:45:20 +0200

    Dalsza praca nad komentarzami projektu

--- src/Media.java
+++ src/Media.java
@@ -1,8 +1,11 @@
+//Abstrakcyjna klasa z ktorej dziedziczy klasa Piosenka
 abstract class Media {
     protected String tytul;
+
     public  Media(String tytul){
         this.tytul = tytul;
     }
+    //Zwracanie tytulu
     public String getTytul(){
         return tytul;
     }
