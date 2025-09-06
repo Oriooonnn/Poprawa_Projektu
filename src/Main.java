a3813a8b090db865f7063bb4d1b60a15dff121c9
Oriooonnn <jasko.kusnierz@vp.pl>
2025-09-06 18:18:23 +0200

    Dalsze prace nad projektem

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,72 @@
 import java.util.Scanner;
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Biblioteka biblioteka = new Biblioteka();
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Library library = new Library();
-    int opcja;
-    do {
-        System.out.println("==MENU GLOWNE SYMULATORA APLIKACJI SPOTIFY==");
-        System.out.println("1. Dodaj nowa piosenke.");
-        System.out.println("2. Wyswietl dostepne utwory.");
-        System.out.println("3. Utworz nowa playliste.");
-        System.out.println("4. Dodaj utwor do playlisty.");
-        System.out.println("5. Wyswietl dostepne playlisty.");
-        System.out.println("6. Odtworz wybrany utwor.");
-        System.out.println("Wybierz opcje.");
-        opcja = scanner.nextInt();
-        scanner.nextLine();
 
-        switch (opcja){
-            case 1:
-                //();
-                break;
-            case 2:
-                //();
-                break;
-            case 3:
-                //();
-                break;
-            case 4:
-                //();
-                break;
-            case 5:
-                //();
-            case 6:
-                //();
-                break;
-            default:
-                System.out.println("Proszę wybrać prawidłową opcje");
+        int opcja;
+        do {
+            System.out.println("==MENU GLOWNE SYMULATORA APLIKACJI SPOTIFY==");
+            System.out.println("1. Dodaj nowa piosenke.");
+            System.out.println("2. Wyswietl dostepne utwory.");
+            System.out.println("3. Utworz nowa playliste.");
+            System.out.println("4. Dodaj utwor do playlisty.");
+            System.out.println("5. Wyswietl dostepne playlisty.");
+            System.out.println("6. Odtworz wybrany utwor.");
+            System.out.println("Wybierz opcje.");
+            opcja = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcja) {
+                case 1:
+                    DodajPiosenke();
+                    break;
+                case 2:
+                    biblioteka.wyswietlPiosenki();
+                    break;
+                case 3:
+                    UtworzPlayliste();
+                    break;
+                case 4:
+                    System.out.println("Podaj nazwę playlisty");
+                    String nazwa = scanner.nextLine();
+                    System.out.println("Podaj numer piosenki");
+                    int numer = scanner.nextInt();
+                    biblioteka.dodajPiosenkeDoPlaylisty(nazwa, numer-1);
+                    break;
+                case 5:
+                    biblioteka.wyswietlPlaylisty();
+                    break;
+                case 6:
+                    odtworzPiosenke();
+                    break;
+                default:
+                    System.out.println("Proszę wybrać prawidłową opcje");
+            }
+        } while (opcja != 0);
+    }
+        private static void DodajPiosenke(){
+        System.out.println("Podaj tytuł piosenki:");
+        String tytul = scanner.nextLine();
+        System.out.println("Podaj wykonawce:");
+        String wykonawca = scanner.nextLine();
+        Piosenka p = new Piosenka(tytul,wykonawca);
+        biblioteka.dodajPiosenke(p);
+        System.out.println("Dodano piosenke " + p);
         }
-    }while (opcja!=0);}}
+        private static void UtworzPlayliste() {
+            System.out.println("Podaj nazwę playlisty:");
+            String nazwa = scanner.nextLine();
+            biblioteka.utworzPlayliste(nazwa);
+        }
+        private static void odtworzPiosenke(){
+        System.out.println("Proszę podac numer piosenki");
+        int numer = scanner.nextInt();
+        Piosenka p = biblioteka.getPiosenka(numer - 1);
+        if(p != null){
+            Player.odtworz(p);
+
+        }else{
+            System.out.println("Nie ma takiej piosenki");
+        }
+        }}
---
+++ src/Player.java
@@ -0,0 +1,6 @@
+public class Player {
+    public static void odtworz(Piosenka p){
+        System.out.println("Odtwarzam piosenke" +  " " + p);
+
+    }
+}
